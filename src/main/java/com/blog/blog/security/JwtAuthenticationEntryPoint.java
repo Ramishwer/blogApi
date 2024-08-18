4712ec5581df5564d0bbf33d8e4094b469c4e5e8
HP <ramishwer3121999@gmail.com>
2024-08-18 22:38:32 +0530

    he

--- src/main/java/com/blog/blog/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/blog/blog/security/JwtAuthenticationEntryPoint.java
@@ -1,13 +1,16 @@
 package com.blog.blog.security;
 
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -16,10 +19,15 @@ import java.io.PrintWriter;
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
 
+//    @Override
+//    public void commence (HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+//
+//
+//        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access Denied!!!");
+//    }
+
     @Override
-    public void commence (HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-
-
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access Denied!!!");
     }
 }
